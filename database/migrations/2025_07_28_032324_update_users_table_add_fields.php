14d04cbca90018dc6da34274539c060f2dc04757
Bao <chelsea691233@gmail.com>
2025-07-28 17:15:37 +0700

    Bài 2: Migration and Models

---
+++ database/migrations/2025_07_28_032324_update_users_table_add_fields.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void // Thay đổi cấu trúc migration của user
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // 1. thêm cột mới
+            $table->string('username')->unique()->after('email'); // Thêm usernme
+            $table->boolean('is_admin')->default(false)->after('username'); // Thêm is_admin
+            $table->boolean('is_active')->default(true)->after('is_admin'); // Thêm is_active
+
+            // 2. đổi 'name' -> 'first_name' + 'last_name'
+            $table->dropColumn('name'); // Xóa cột name
+            $table->string('first_name')->after('id'); // Thêm first_name
+            $table->string('last_name')->after('first_name'); // Thêm lase_name
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void // Đảo ngược thay đổi (xóa bỏ, hoàn tác)
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->dropColumn(['username', 'is_admin', 'is_active', 'first_name', 'last_name']);
+            $table->string('name')->after('email');
+        });
+    }
+};
