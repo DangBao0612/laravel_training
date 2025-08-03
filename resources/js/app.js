cf140e507185819c93540a72638868fe1e6e04a5
Bao <chelsea691233@gmail.com>
2025-08-04 00:17:38 +0700

    Bài 10: viết file css,js,scss trong Resources và compile vào Public

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
-import './bootstrap';
-
-import Alpine from 'alpinejs';
-
+import "./bootstrap"     // Laravel Breeze / Sanctum bootstrap
+import Alpine from "alpinejs";
 window.Alpine = Alpine;
-
 Alpine.start();
